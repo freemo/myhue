29379255a72856f3bf4ce3614be99a4c510c72e6
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2018-05-14 06:24:58 -0400

    temperature color gradient.

--- src/weather-to-lights.js
+++ src/weather-to-lights.js
@@ -8,20 +8,37 @@ let getOnLights = require('./get-on-lights');
 let sleep = require('./sleep');
 let retry = require('./retry');
 
+const MAX_TEMP = 90.0;
+const MIN_TEMP = 32.0;
+const NOMINAL_TEMP = 68.0;
 const COLD_XY = [0.1355, 0.0399];
-const NORMAL_XY = [0.477, 0.5042];
+const NOMINAL_XY = [0.35, 0.25];
 const HOT_XY = [0.7006, 0.2993];
 
-getWeather(weather => {
-  let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-  console.log(message);
+function getTempColorXy(temp) {
+  let tempAdj = temp;
+  if( tempAdj > MAX_TEMP )
+    tempAdj = MAX_TEMP;
+  else if(tempAdj < MIN_TEMP)
+    tempAdj = MIN_TEMP;
 
-  let tempXy = NORMAL_XY;
-  if(weather.main.temp > 74) {
-    tempXy = HOT_XY;
-  } else if( weather.main.temp < 62 ) {
-    tempXy = COLD_XY;
+  let tempPerc = 0.0;
+  if( tempAdj > NOMINAL_TEMP ) {
+    let tempPerc = Math.abs((tempAdj - NOMINAL_TEMP) / (MAX_TEMP - NOMINAL_TEMP));
+    let x = NOMINAL_XY[0] + (NOMINAL_XY[0] < HOT_XY[0] ? 1 : -1) * Math.abs(NOMINAL_XY[0] - HOT_XY[0]) * tempPerc;
+    let y = NOMINAL_XY[1] + (NOMINAL_XY[1] < HOT_XY[1] ? 1 : -1) * Math.abs(NOMINAL_XY[1] - HOT_XY[1]) * tempPerc;
+    return [x, y];
+  } else if( tempAdj < NOMINAL_TEMP ) {
+    let tempPerc = Math.abs((tempAdj - NOMINAL_TEMP) / (NOMINAL_TEMP - MIN_TEMP));
+    let x = NOMINAL_XY[0] + (NOMINAL_XY[0] < COLD_XY[0] ? 1 : -1) * Math.abs(NOMINAL_XY[0] - COLD_XY[0]) * tempPerc;
+    let y = NOMINAL_XY[1] + (NOMINAL_XY[1] < COLD_XY[1] ? 1 : -1) * Math.abs(NOMINAL_XY[1] - COLD_XY[1]) * tempPerc;
+    return [x, y];
   }
+  return NOMINAL_XY;
+}
+
+getWeather(weather => {
+  let tempXy = getTempColorXy(weather.main.temp);
 
   client.lights.getAll()
     .then(lights => {
